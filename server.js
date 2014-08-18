e13f0957dceab415800fe12f779901ed1bdb9a20
Mandi Grant <mandi018@gmail.com>
2014-08-18 10:10:55 -0700

    Rewrote the server var in hopes of fixing heroku deploy

--- server.js
+++ server.js
@@ -20,8 +20,15 @@ app.use(bodyparser.json()); //necessary for mocha chai tests
 app.use(bodyparser.urlencoded({ extended: false })); //necessary for front end html
 require('./routes/note-routes')(app); //require note-routes (which is exported from same named file) 
 
+/*
 var server = http.createServer(app);
 
 server.listen(3000, function() {
   console.log('server runnning on port 3000');
+});
+*/
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
